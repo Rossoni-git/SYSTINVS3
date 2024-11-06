8db6f1f5e2a0460efb38e226849a024e59d9ec5c
Rossoni-git <joaovictorrossoni094@gmail.com>
2024-11-06 16:14:43 -0300

    Metodos construtores terminados

--- SysTINSClass/Cliente.cs
+++ SysTINSClass/Cliente.cs
@@ -15,11 +15,49 @@ namespace SysTINSClass
         public string? Nome { get; set; }
         public string Cpf { get; set; }
         public string? Telefone { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public DateTime Data_nasc { get; set; }
-        public List<Endereco> Endereco { get; set; }
+        public bool Ativo { get; set; }
 
+        public List<Endereco>? Endereco { get; set; }
 
+        public Cliente(){
 
+           Endereco = new(); 
+            }
+        //Metodo construtor
+        public Cliente(int id, string? nome, string cpf, string? telefone, string email, DateTime data_nasc,bool ativo, List<Endereco> endereco)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            Data_nasc = data_nasc;
+            Ativo = ativo;
+            Endereco = endereco;
+        }
+
+        //Metodo construtor
+        public Cliente( string? nome, string cpf, string? telefone, string email, DateTime data_nasc)
+        {
+           
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            Data_nasc = data_nasc;
+        
+        }
+        //Metodo construtor
+        public Cliente(int id, string? nome, string cpf, string? telefone, DateTime data_nasc)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Data_nasc = data_nasc;
+
+        }
     }
 }
