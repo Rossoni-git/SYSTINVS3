fe7943daae70657d9681965c4a9e4cc5cdfea022
Rossoni-git <joaovictorrossoni094@gmail.com>
2024-11-06 17:18:22 -0300

    Começando o método insert do cliente

---
+++ SysTINSClass/Enderços.cs
@@ -0,0 +1,91 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysTINSClass
+{
+    //Criação das propriedades 
+   public class Endereco
+    {
+     
+
+        public int Id { get; set; }
+        public Cliente? Cliente { get; set; }
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+        public string? Uf { get; set; }
+        public string ? Tipo_endereco { get; set; }
+
+        //Métodos construtores
+        public Endereco(int id, Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento, string? bairro, string? cidade, string? uf, string? tipo_endereco)
+        {
+            Id = id;
+            Cliente = cliente;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            Tipo_endereco = tipo_endereco;
+        }
+        public Endereco( Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento, 
+            string? bairro, string? cidade, string? uf, string? tipo_endereco)
+        {
+
+            Cliente = cliente;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            Tipo_endereco = tipo_endereco;
+        }
+        //Métodos Construtores
+        public Endereco(int id, string? cep, string? logradouro, string? numero, string? complemento,
+           string? bairro, string? cidade, string? uf, string? tipo_endereco)
+        {
+
+            Id = id;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = cidade;
+            Uf = uf;
+            Tipo_endereco = tipo_endereco;
+        }
+
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_enderecos_insert";
+            cmd.Parameters.AddWithValue("spcliente_id", Cliente.Id);
+            cmd.Parameters.AddWithValue("spcpf",);
+            cmd.Parameters.AddWithValue("sptelefone", Telefone);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.Parameters.AddWithValue("spdatanasc", Data_nasc);
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Id = dr.GetInt32(0);
+
+            }
+            cmd.Connection.Close();
+
+        }
+}
