3146269bd74bbbd5a810233cef8aae65d1d97179
Rossoni-git <joaovictorrossoni094@gmail.com>
2024-11-08 17:21:48 -0300

    Termineio o atualizar e comecei o obter lista

--- SysTINSClass/Enderços.cs
+++ SysTINSClass/Enderços.cs
@@ -9,9 +9,9 @@ using System.Threading.Tasks;
 namespace SysTINSClass
 {
     //Criação das propriedades 
-   public class Endereco
+    public class Endereco
     {
-     
+
 
         public int Id { get; set; }
         public Cliente? Cliente { get; set; }
@@ -22,7 +22,7 @@ namespace SysTINSClass
         public string? Bairro { get; set; }
         public string? Cidade { get; set; }
         public string? Uf { get; set; }
-        public string ? Tipo_endereco { get; set; }
+        public string? Tipo_endereco { get; set; }
 
         //Métodos construtores
         public Endereco(int id, Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento, string? bairro, string? cidade, string? uf, string? tipo_endereco)
@@ -38,7 +38,7 @@ namespace SysTINSClass
             Uf = uf;
             Tipo_endereco = tipo_endereco;
         }
-        public Endereco( Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento, 
+        public Endereco(Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento,
             string? bairro, string? cidade, string? uf, string? tipo_endereco)
         {
 
@@ -74,11 +74,14 @@ namespace SysTINSClass
 
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.CommandText = "sp_enderecos_insert";
-            cmd.Parameters.AddWithValue("spcliente_id", Cliente.Id);
-            cmd.Parameters.AddWithValue("spcpf",);
-            cmd.Parameters.AddWithValue("sptelefone", Telefone);
-            cmd.Parameters.AddWithValue("spemail", Email);
-            cmd.Parameters.AddWithValue("spdatanasc", Data_nasc);
+            cmd.Parameters.AddWithValue("spcep", Cep);
+            cmd.Parameters.AddWithValue("splogradouro", Logradouro);
+            cmd.Parameters.AddWithValue("spnumero", Numero);
+            cmd.Parameters.AddWithValue("spcomplemento", Complemento);
+            cmd.Parameters.AddWithValue("spbairro", Bairro);
+            cmd.Parameters.AddWithValue("spcidade", Cidade);
+            cmd.Parameters.AddWithValue("spuf", Uf);
+            cmd.Parameters.AddWithValue("sptipo", Tipo_endereco);
             var dr = cmd.ExecuteReader();
             if (dr.Read())
             {
@@ -88,4 +91,65 @@ namespace SysTINSClass
             cmd.Connection.Close();
 
         }
+        public bool Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_endereco_altera";
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.Parameters.AddWithValue("spcep", Cep);
+            cmd.Parameters.AddWithValue("splogradouro", Logradouro);
+            cmd.Parameters.AddWithValue("spnumero", Numero);
+            cmd.Parameters.AddWithValue("spcomplemento", Complemento);
+            cmd.Parameters.AddWithValue("spbairro", Bairro);
+            cmd.Parameters.AddWithValue("spcidade", Cidade);
+            cmd.Parameters.AddWithValue("spuf", Uf);
+            cmd.Parameters.AddWithValue("sptipo_endereco", Tipo_endereco);
+
+
+            return cmd.ExecuteNonQuery() > 0 ? true : false;
+        }
+        // efetuar login
+        public static Usuario EfetuarLogin(string email, string senha)
+        {
+            Usuario usuario = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from usuarios where email = '{email}' and senha = md5('{senha}') and ativo = 1";
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                usuario = new(
+                        dr.GetInt32(0),
+                        dr.GetString(1),
+                        dr.GetString(2),
+                        dr.GetString(3),
+                        Nivel.ObterPorId(dr.GetInt32(4)),
+                        dr.GetBoolean(5)
+                    );
+            }
+            return usuario;
+
+        }
+        public void Excluir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "sp_endereco_delete";
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+        }
+        public static List<Endereco> ObterLista()
+        {
+
+
+
+            return ObterLista();
+        }
+        
+       
+
+
+
+    }
+
 }
