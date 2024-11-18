57793184ca9cbbbaf2a4d865c6133754534e3ae8
Rossoni-git <joaovictorrossoni094@gmail.com>
2024-11-18 16:05:58 -0300

    Classe item pedido terminada

--- SysTINSClass/Pedido.cs
+++ SysTINSClass/Pedido.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace SysTINSClass
 {
-    internal class Pedido
+    public class Pedido
     {
+       
+
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public  Cliente Cliente { get; set; }
+        public DateTime Data { get; set; }
+        public string? Status { get; set; }
+        public decimal Desconto { get; set; }
+
+
+
+
+
+        public Pedido()
+        {
+            Cliente = new();
+            Usuario = new();
+
+
+        }
+        public Pedido( Usuario usuario, Cliente cliente)
+        {
+           
+            Usuario = usuario;
+            Cliente = cliente;
+          
+        }
+        public Pedido(int id, Usuario usuario, Cliente cliente, DateTime data, string status, decimal desconto)
+        {
+            Id = id;
+            Usuario = usuario;
+            Cliente = cliente;
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+        }
+
     }
 }
