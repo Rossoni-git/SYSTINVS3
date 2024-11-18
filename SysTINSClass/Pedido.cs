b9c472ec5fa5efe48db210ac9d055b3a9dc46362
Rossoni-git <joaovictorrossoni094@gmail.com>
2024-11-18 16:44:26 -0300

    Classe do pedido terminada

--- SysTINSClass/Pedido.cs
+++ SysTINSClass/Pedido.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,16 @@ namespace SysTINSClass
 {
     public class Pedido
     {
-       
+
 
         public int Id { get; set; }
         public Usuario Usuario { get; set; }
-        public  Cliente Cliente { get; set; }
+        public Cliente Cliente { get; set; }
         public DateTime Data { get; set; }
         public string? Status { get; set; }
-        public decimal Desconto { get; set; }
+        public double Desconto { get; set; }
 
-
+        public List<ItemPedido>? Items { get; set; }
 
 
 
@@ -28,14 +29,14 @@ namespace SysTINSClass
 
 
         }
-        public Pedido( Usuario usuario, Cliente cliente)
+        public Pedido(Usuario usuario, Cliente cliente)
         {
-           
+
             Usuario = usuario;
             Cliente = cliente;
-          
+
         }
-        public Pedido(int id, Usuario usuario, Cliente cliente, DateTime data, string status, decimal desconto)
+        public Pedido(int id, Usuario usuario, Cliente cliente, DateTime data, string status, double desconto)
         {
             Id = id;
             Usuario = usuario;
@@ -44,6 +45,93 @@ namespace SysTINSClass
             Status = status;
             Desconto = desconto;
         }
+        public Pedido(int id, Usuario usuario, Cliente cliente, DateTime data, string status, double desconto, List<ItemPedido> items)
+        {
+            Id = id;
+            Usuario = usuario;
+            Cliente = cliente;
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+            Items = items;
+        }
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_pedido_insert";
+            cmd.Parameters.AddWithValue("spusuario_id", Usuario.Id);
+            cmd.Parameters.AddWithValue("spcliente_id", Cliente.Id);
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+        }
+
+        public void Atualizar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_pedido_update";
+            cmd.Parameters.AddWithValue("spid", Id);
+            cmd.Parameters.AddWithValue("spstatus", Status);
+            cmd.Parameters.AddWithValue("spdesconto", Desconto);
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+
+
+        }
+        public static Pedido ObterPorId(int id)
+        {
+            Pedido pedido = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from pedidos where id = {id}";
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+
+            {
+                pedido = new(
+                    dr.GetInt32(0),
+                    Usuario.ObterPorId(dr.GetInt32(1)),
+                    Cliente.ObterporId(dr.GetInt32(2)),
+                    dr.GetDateTime(3),
+                    dr.GetString(4),
+                    dr.GetDouble(5),
+                    ItemPedido.ObterItensPorPedidoId(dr.GetInt32(0))
+
+                    );
+
+
+            }
+
+            return pedido;
+
+
+
+        }
+        public static List<Pedido> ObterLista()
+        {
+            List<Pedido> pedidos = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from pedidos" ;
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+
+            {
+                pedidos.Add(new(
+                    dr.GetInt32(0),
+                    Usuario.ObterPorId(dr.GetInt32(1)),
+                    Cliente.ObterporId(dr.GetInt32(2)),
+                    dr.GetDateTime(3),
+                    dr.GetString(4),
+                    dr.GetDouble(5),
+                    ItemPedido.ObterItensPorPedidoId(dr.GetInt32(0))
+
+                    ));
+            }
+            return pedidos;
+
+        }
 
     }
-}
+
+ }
